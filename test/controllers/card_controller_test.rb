96164aa0832c94baa3725233e85d7435d63940e6
Raphael Molesim <raphael.sm86@gmail.com.br>
2016-11-19 22:54:25 -0200

    Removed unit test directory

--- test/controllers/card_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CardControllerTest < ActionDispatch::IntegrationTest
-  test "should get search" do
-    get card_search_url
-    assert_response :success
-  end
-
-end
--- test/controllers/session_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SessionControllerTest < ActionDispatch::IntegrationTest
-  test "should get logout" do
-    get session_logout_url
-    assert_response :success
-  end
-
-end
